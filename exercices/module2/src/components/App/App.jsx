9960b25f9361565077932741e1316550d68dbd7e
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2023-09-22 11:31:37 +0200

    module2 : localStorage

--- exercices/module2/src/components/App/App.jsx
+++ exercices/module2/src/components/App/App.jsx
@@ -3,12 +3,13 @@ import Button from "components/Button/Button.jsx";
 import {useState} from "react";
 
 const App = () => {
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(JSON.parse(localStorage.getItem("counter")))
 
     console.log('rendering with counter value', counter)
 
     const changeCount = (delta) => {
         setCounter(counter + delta)
+        localStorage.setItem("counter", JSON.stringify(counter))
     }
 
     return (
