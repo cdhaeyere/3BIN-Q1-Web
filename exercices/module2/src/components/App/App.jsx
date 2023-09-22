aa0867ec423f7eb8320ac3043c05504bbbe39443
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2023-09-22 11:15:40 +0200

    module2 : init

---
+++ exercices/module2/src/components/App/App.jsx
@@ -0,0 +1,34 @@
+import Display from "components/Display/Display.jsx";
+import Button from "components/Button/Button.jsx";
+import {useState} from "react";
+
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    
+    console.log('rendering with counter value', counter)
+    const increaseByOne = () => {
+        console.log('increasing, value before', counter);
+        setCounter(counter + 1)
+    }
+
+    const decreaseByOne = () => {
+        console.log('decreasing, value before', counter);
+        setCounter(counter - 1)
+    }
+
+    const setToZero = () => {
+        console.log('resetting to zero, value before', counter);
+        setCounter(0)
+    }
+
+    return (
+        <div>
+            <Display counter={counter} />
+            <Button handleClick={increaseByOne} text="plus" />
+            <Button handleClick={setToZero} text="zero" />
+            <Button handleClick={decreaseByOne} text="minus" />
+        </div>
+    )
+}
+
+export default App
