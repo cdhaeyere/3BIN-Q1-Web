7fc398a03bb9f652e4a7462b65947aed6edb6e31
cdhaeyere <corentin.dhaeyere@student.vinci.be>
2023-09-30 14:58:29 +0200

    module3 : loading

--- exercices/module3/3.1/src/components/App/App.jsx
+++ exercices/module3/3.1/src/components/App/App.jsx
@@ -1,11 +1,17 @@
 import Button from 'components/Button/Button';
 import Statistics from 'components/Statistics/Statistics';
 import { useState } from 'react';
+import Loading from 'components/Loading/Loading';
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => setLoading(false), 3000);
+
+  if (loading) return <Loading />;
 
   const handleClick = (e) => {
     if (e.target.className === 'good') setGood(good + 1);
